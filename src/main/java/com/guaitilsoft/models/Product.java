224144b4c0f17fbe43989cc76c86080d507d3ad8
tatimoramen <tatimorales02@icloud.com>
2020-08-16 14:53:03 -0600

    modelo loca

--- src/main/java/com/guaitilsoft/models/Product.java
+++ src/main/java/com/guaitilsoft/models/Product.java
@@ -27,7 +27,7 @@ public class Product {
     @NotEmpty
     private String description;
 
-    @NotEmpty
+    @Column
     private Boolean status;
 
     @Enumerated(EnumType.STRING)
@@ -37,7 +37,7 @@ public class Product {
     @JoinColumn(name="local_id")
     private Local local;
 
-    @OneToMany(fetch = FetchType.EAGER)
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Multimedia> multimedia;
 
     @OneToOne
