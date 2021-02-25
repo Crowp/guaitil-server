9a708c9522886e079ba23bd8a51f4b95c7cbe617
itsRicmor <ricardojms1999@gmail.com>
2021-02-25 12:13:27 -0600

    Add date formatter to member DTO

--- src/main/java/com/guaitilsoft/web/models/member/MemberView.java
+++ src/main/java/com/guaitilsoft/web/models/member/MemberView.java
@@ -1,5 +1,6 @@
 package com.guaitilsoft.web.models.member;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.guaitilsoft.models.Person;
 import com.guaitilsoft.models.constant.MemberType;
 import com.guaitilsoft.web.models.local.LoadLocal;
@@ -17,8 +18,10 @@ public class MemberView {
 
     private String occupation;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 
     private Person person;
