2a90f3f4dfad302ee265f718ebe186bf1c60b5c9
Luis <luis.rodriguez.baltodano@est.una.ac.cr>
2021-04-07 16:25:09 -0600

    Tarea activity creada

--- src/main/java/com/guaitilsoft/repositories/ActivityRepository.java
+++ src/main/java/com/guaitilsoft/repositories/ActivityRepository.java
@@ -1,9 +1,18 @@
 package com.guaitilsoft.repositories;
 
 import com.guaitilsoft.models.Activity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
+
+    @Query("SELECT a FROM Activity a WHERE a.activityDescription.id IN " +
+            "(SELECT ad FROM ActivityDescription ad WHERE  ad.activityDate < :activityDate) AND a.isActive=true")
+    List<Activity> getActivitiesDone(@Param("activityDate") LocalDateTime activityDate);
 }
--- src/main/java/com/guaitilsoft/scheduledTasks/ScheduledTask.java
+++ src/main/java/com/guaitilsoft/scheduledTasks/ScheduledTask.java
@@ -1,8 +1,10 @@
 package com.guaitilsoft.scheduledTasks;
 
+import com.guaitilsoft.models.Activity;
 import com.guaitilsoft.models.ActivityDescription;
 import com.guaitilsoft.models.LocalDescription;
 import com.guaitilsoft.models.ProductDescription;
+import com.guaitilsoft.repositories.ActivityRepository;
 import com.guaitilsoft.services.activityDescription.ActivityDesRepositoryService;
 import com.guaitilsoft.services.localDescription.LocalDesRepositoryService;
 import com.guaitilsoft.services.productDescription.ProductDesRepositoryService;
@@ -24,17 +26,20 @@ public class ScheduledTask {
     private final LocalDesRepositoryService localDescriptionRepository;
     private final ProductDesRepositoryService productDescriptionRepository;
     private final ActivityDesRepositoryService activityDescriptionRepository;
+    private final ActivityRepository activityRepository;
 
     @Autowired
     public ScheduledTask(LocalDesRepositoryService localDescriptionRepository,
                          ProductDesRepositoryService productDescriptionRepository,
-                         ActivityDesRepositoryService activityDescriptionRepository) {
+                         ActivityDesRepositoryService activityDescriptionRepository,
+                         ActivityRepository activityRepository) {
         this.localDescriptionRepository = localDescriptionRepository;
         this.productDescriptionRepository = productDescriptionRepository;
         this.activityDescriptionRepository = activityDescriptionRepository;
+        this.activityRepository = activityRepository;
     }
 
-    @Scheduled(cron = "0 0 23 31 * ?")
+    @Scheduled(cron = "30 1 31 * *")
     public void deleteLocalDescriptionWithoutRelationship(){
         List<LocalDescription> localDescriptions = localDescriptionRepository.getLocalsDescriptionNoRelationships();
         if (localDescriptions.size() != 0) {
@@ -43,7 +48,7 @@ public class ScheduledTask {
         }
     }
 
-    @Scheduled(cron = "0 0 23 30 * ?")
+    @Scheduled(cron = "0 0 31 * *")
     public void deleteProductDescriptionWithoutRelationship(){
         List<ProductDescription> productDescriptions = productDescriptionRepository.getLocalsDescriptionNoRelationships();
         if (productDescriptions.size() != 0) {
@@ -52,12 +57,26 @@ public class ScheduledTask {
         }
     }
 
-    @Scheduled(cron = "0 0 23 29 * ?")
+    @Scheduled(cron = "0 1 31 * *")
     public void deleteActivityDescriptionWithoutRelationship(){
         List<ActivityDescription> activityDescriptions = activityDescriptionRepository.getActivityDescriptionNoRelationships();
         if (activityDescriptions.size() != 0) {
             activityDescriptions.forEach(activityDescription -> activityDescriptionRepository.delete(activityDescription.getId()));
             logger.info("Activities descriptions deleted {}", dateTimeFormatter.format(LocalDateTime.now()) );
+        }
+    }
+
+    //@Scheduled(cron = "1 * * * *")
+    @Scheduled(cron = "30 23 * * 0")
+    public void updateActivityIsActive(){
+        List<Activity> activities = activityRepository.getActivitiesDone(LocalDateTime.now());
+
+        if (activities.size() != 0){
+            activities.forEach(activity -> {
+                    activity.setIsActive(!activity.getIsActive());
+                    activityRepository.save(activity);
+                    logger.info("Activity update isActive {}", dateTimeFormatter.format(LocalDateTime.now()) );
+            });
         }
     }
 }
