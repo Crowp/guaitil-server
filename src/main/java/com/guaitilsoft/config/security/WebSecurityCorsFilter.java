41332820f83a28882b8c1be23ffde21ac3d7bb0d
itsRicmor <ricardojms1999@gmail.com>
2021-04-21 13:37:28 -0600

    prueba

--- src/main/java/com/guaitilsoft/config/security/WebSecurityCorsFilter.java
+++ src/main/java/com/guaitilsoft/config/security/WebSecurityCorsFilter.java
@@ -1,29 +1,21 @@
 package com.guaitilsoft.config.security;
 
-import org.springframework.beans.factory.annotation.Value;
-
 import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class WebSecurityCorsFilter implements Filter {
-
-    @Value("${guaitil-client.cors}")
-    private String urlCors;
-
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", this.urlCors);
-        response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Allow-Credentials", String.valueOf(true));
-        response.setHeader("Access-Control-Max-Age", String.valueOf(180));
-        filterChain.doFilter(servletRequest, servletResponse);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "*");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "*");
+        chain.doFilter(request, res);
     }
     @Override
     public void destroy() {
