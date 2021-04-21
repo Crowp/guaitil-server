f83d2c37ff24bc40a31b45e3d54963e638098c67
itsRicmor <ricardojms1999@gmail.com>
2021-04-21 12:18:25 -0600

    add cors on production

--- src/main/java/com/guaitilsoft/config/security/WebSecurityCorsFilter.java
+++ src/main/java/com/guaitilsoft/config/security/WebSecurityCorsFilter.java
@@ -1,20 +1,26 @@
 package com.guaitilsoft.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class WebSecurityCorsFilter implements Filter {
+
+    @Value("${guaitil-client.cors}")
+    private String urlCors;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "*");
+        res.setHeader("Access-Control-Allow-Origin", this.urlCors);
+        res.setHeader("Access-Control-Allow-Methods", this.urlCors);
         res.setHeader("Access-Control-Max-Age", "3600");
-        res.setHeader("Access-Control-Allow-Headers", "*");
+        res.setHeader("Access-Control-Allow-Headers", this.urlCors);
         chain.doFilter(request, res);
     }
     @Override
